fa84787679b26e63b400e62b97b0ff30ba4b7c52
okekolawolesunday009 <okekolawolesunday@gmail.com>
2025-12-02 02:59:34 +0100

    terraform

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "devops-stage6-terraform-state-8bb03bd2"
+    bucket         = "devops-stage-6-terraform-state"
     key            = "devops-app/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
